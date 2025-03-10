c89d3f96df5b3461554c60b5e95e7704d016dab4
DESKTOP-F0GH6SG\USER <DESKTOP-F0GH6SG\USER>
2025-03-10 12:07:48 +0800

    masm

---
+++ week 4/Main.asm
@@ -0,0 +1,45 @@
+.386
+.model flat, stdcall
+.stack 4096
+
+
+include Irvine32.inc
+;includelib Irvine32.lib
+;includelib kernel32.lib
+;includelib user32.lib
+ExitProcess PROTO, dwExitCode:DWORD
+
+.data
+nums byte 5 dup(?)
+
+.code
+main PROC
+	;init
+	mov ecx, lengthof nums
+	mov edi, type nums
+	mov eax, 0
+	mov edi, 0
+
+L1:
+	mov eax, 50
+	call RandomRange
+	mov nums[edi], al
+	add edi, type nums
+	loop L1
+
+	mov edi, offset nums
+	mov eax, 0
+	mov edi, 0
+	mov ecx, lengthof nums
+
+L2:
+	mov al, nums[edi]
+	add edi, type nums
+	call WriteInt
+	call Crlf
+	loop L2
+
+	INVOKE ExitProcess, eax
+main ENDP
+
+END main        ;specify the program's entry point
